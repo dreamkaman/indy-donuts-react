e1256cc57af45ff12948dcc4a44cd8461ccfed26
Dreamkaman <riabushko@gmail.com>
2023-11-14 01:43:51 +0200

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import HomePage from 'pages/HomePage';
 import './App.css';
+import Header from 'components/Header';
 
 function App() {
-  return <HomePage></HomePage>;
+  return (
+    <HomePage>
+      <Header />
+    </HomePage>
+  );
 }
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,30 @@
+import { Link } from 'react-router-dom';
+
+import GetSvg from 'shared/components/GetSvg';
+import s from './Header.module.scss';
+
+
 const Header = () => {
-    return <p>Hello Header!</p>;
+    return <div className={s.appHeader}>
+        <GetSvg name={'icon-logo'} className={s.logo} />
+        <GetSvg name={'icon-burger'} className={s.burgerMenu} />
+        <nav>
+            <ul className={s.menu}>
+                <li>
+                    <Link to={""}>About Us</Link>
+                </li>
+                <li>
+                    <Link to={""}>Program</Link>
+                </li>
+                <li>
+                    <Link to={""}>Reviews</Link>
+                </li>
+                <li>
+                    <Link to={""}>Contacts</Link>
+                </li>
+            </ul>
+        </nav>
+    </div>;
 };
 
 export default Header;
