7d26aba7dfb95c8c97992c330b7d9010d3b4791a
Dreamkaman <riabushko@gmail.com>
2023-11-16 01:02:50 +0200

    Next

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -2,20 +2,19 @@ import Paragraph from 'shared/components/Paragraph';
 import Section from 'shared/components/Section';
 
 import img from 'images/png/donut-set.png';
+import Picture from 'shared/components/Picture/Picture';
 
 const AboutUs = () => {
-    const text = `Oh My Donut is the shop which adores tasty deserts and spend hours to create sophisticated
-                donuts which will save the world!
-                Eva has been baking since she was six years old and over the last 49 years, she has created
-                hundreds of cakes for weddings and has baked for many celebrities. Eva and our shop have
-                received multiple awards & recognition for excellence.
-                We are going to teach you how to cook with passion and sparkles in your eyes! We love what
-                we do and believe you will enjoy our cooking atmosphere!`
+    const text = `Oh My Donut is the shop which adores tasty deserts and spend hours to create sophisticated donuts which will save the world!
+
+                Eva has been baking since she was six years old and over the last 49 years, she has created hundreds of cakes for weddings and has baked for many celebrities. Eva and our shop have received multiple awards & recognition for excellence.
+
+                We are going to teach you how to cook with passion and sparkles in your eyes! We love what we do and believe you will enjoy our cooking atmosphere!`
 
     return (
-        <Section title={'About Us'}>
-            <img src={img} alt="" />
-            <Paragraph text={text} />
+        <Section>
+            <Picture imgPath={img} imgAlt={'donuts set'} />
+            <Paragraph title={'About Us'} text={text} />
         </Section>
     );
 };
