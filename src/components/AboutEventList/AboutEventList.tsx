3dbcae6ff7576f64879096c3b5bf9a2c5d7b42cb
Dreamkaman <riabushko@gmail.com>
2023-11-20 22:02:14 +0200

    Next

--- src/components/AboutEventList/AboutEventList.tsx
+++ src/components/AboutEventList/AboutEventList.tsx
@@ -8,40 +8,49 @@ import donut4 from 'images/png/donut-4.png';
 import s from './AboutEventList.module.scss';
 
 const AboutEventList = () => {
-    return <div className={s.aboutEventList}>
-        <AboutEventItem
-            imgAlt={'donut'}
-            imgPath={donut1}
-            title={'Type of the event'}
-            text={'Cooking classes'}
-            svgName={'icon-chef-hat'}
-            svgClassName={s.chefHat}
-        />
-        <AboutEventItem
-            imgAlt={'donut'}
-            imgPath={donut2}
-            title='Number of guests'
-            text='1 to 20'
-            svgName={'icon-people'}
-            svgClassName={s.iconPeople}
-        />
-        <AboutEventItem
-            imgAlt={'donut'}
-            imgPath={donut3}
-            title={'Time of the event'}
-            text={'18:30 (2.5 hours)'}
-            svgName={'icon-clock'}
-            svgClassName={s.iconClock}
-        />
-        <AboutEventItem
-            imgAlt={'donut'}
-            imgPath={donut4}
-            title={'The host speaks'}
-            text={'English, French'}
-            svgName={'icon-chat'}
-            svgClassName={s.iconChat}
-        />
-    </div>
+    return <ul className={s.aboutEventList}>
+        <li className={s.aboutEventListItem}>
+            <AboutEventItem
+                imgAlt={'donut'}
+                imgPath={donut1}
+                title={'Type of the event'}
+                text={'Cooking classes'}
+                svgName={'icon-chef-hat'}
+                svgClassName={s.chefHat}
+            />
+        </li>
+
+        <li className={s.aboutEventListItem}>
+            <AboutEventItem
+                imgAlt={'donut'}
+                imgPath={donut2}
+                title='Number of guests'
+                text='1 to 20'
+                svgName={'icon-people'}
+                svgClassName={s.iconPeople}
+            />
+        </li>
+        <li className={s.aboutEventListItem}>
+            <AboutEventItem
+                imgAlt={'donut'}
+                imgPath={donut3}
+                title={'Time of the event'}
+                text={'18:30 (2.5 hours)'}
+                svgName={'icon-clock'}
+                svgClassName={s.iconClock}
+            />
+        </li>
+        <li className={s.aboutEventListItem}>
+            <AboutEventItem
+                imgAlt={'donut'}
+                imgPath={donut4}
+                title={'The host speaks'}
+                text={'English, French'}
+                svgName={'icon-chat'}
+                svgClassName={s.iconChat}
+            />
+        </li>
+    </ul>
 }
 
 export default AboutEventList;
