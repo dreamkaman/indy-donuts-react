866063372690cf31f5cd7f3b003ba14ab29847d4
Dreamkaman <riabushko@gmail.com>
2023-11-21 23:23:37 +0200

    Next

---
+++ src/components/Reviews/Reviews.tsx
@@ -0,0 +1,11 @@
+import Section from "shared/components/Section";
+import Slider from "shared/components/Slider";
+
+const Reviews = () => {
+    return <Section>
+        <p>Hello Reviews!</p>
+        <Slider slides={[]} />
+    </Section>
+}
+
+export default Reviews;
